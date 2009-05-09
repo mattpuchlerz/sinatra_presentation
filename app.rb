7c83f0612d37fb13fa719412015b6cc0cfa92c7c
Matt Puchlerz <matt@puchlerz.com>
2009-05-09 11:31:17 -0500

    Added the standard CRUD and form routes.

--- app.rb
+++ app.rb
@@ -1,6 +1,38 @@
 require 'rubygems'
 require 'sinatra'
 
-get '/' do
-  'yo dawg'
+# CREATE
+
+get '/bowling_games/new' do
+  "get: form for a new game"
+end
+
+post '/bowling_games' do
+  "post: create a new game"
+end
+
+# READ
+
+get '/bowling_games' do
+  "get: read ALL games"
+end
+
+get '/bowling_games/:id' do
+  "get: read game '#{params[:id]}'"
+end
+
+# UPDATE
+
+get '/bowling_games/:id/edit' do
+  "get: form for editing game '#{params[:id]}'"
+end
+
+put '/bowling_games' do
+  "put: update a game"
+end
+
+# DELETE
+
+delete '/bowling_games' do
+  "delete: delete a game"
 end
