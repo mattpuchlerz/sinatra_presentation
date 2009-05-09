866396b126cbfa0a3ffd0da708655f8387aaca45
Matt Puchlerz <matt@puchlerz.com>
2009-05-09 12:20:18 -0500

    Reorganized the routes, and added a redirection for '/'

--- app.rb
+++ app.rb
@@ -1,37 +1,33 @@
 require 'rubygems'
 require 'sinatra'
 
-# CREATE
-
-get '/bowling_games/new' do
-  "get: form for a new game"
+get '/' do
+  redirect '/bowling_games'
 end
-
-post '/bowling_games' do
-  "post: create a new game"
-end
-
-# READ
 
 get '/bowling_games' do
   "get: read ALL games"
+end
+
+get '/bowling_games/new' do
+  "get: form for a new game"
 end
 
 get '/bowling_games/:id' do
   "get: read game '#{params[:id]}'"
 end
 
-# UPDATE
-
 get '/bowling_games/:id/edit' do
   "get: form for editing game '#{params[:id]}'"
+end
+
+post '/bowling_games' do
+  "post: create a new game"
 end
 
 put '/bowling_games' do
   "put: update a game"
 end
-
-# DELETE
 
 delete '/bowling_games' do
   "delete: delete a game"
